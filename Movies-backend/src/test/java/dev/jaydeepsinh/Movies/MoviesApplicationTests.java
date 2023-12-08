aa892b79a11ea277c88c73077566f3043d8e0482
jaydeepsinh9 <jaydeepsinh1484@gmail.com>
2023-12-07 20:24:38 -0500

    Initial commit

---
+++ Movies-backend/src/test/java/dev/jaydeepsinh/Movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jaydeepsinh.Movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
